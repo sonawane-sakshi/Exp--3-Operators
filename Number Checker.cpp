b9c8c437c4443b0e046a5b93a76cfb5ebf0687fb
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 11:31:50 +0530

    Add files via upload

---
+++ Number Checker.cpp
@@ -0,0 +1,17 @@
+//Sakshi Sonawane 
+//PRN:24070123505
+//experiment 3(1)
+//program to check if a number is positive or negative
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    if (num>=0){
+        cout<<num<<" is positive";
+    }
+    else {
+        cout<<num<<" is negative";
+    }
+}
