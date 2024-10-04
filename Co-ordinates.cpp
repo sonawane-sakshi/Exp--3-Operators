cc6574701aee99a6eee5bdd2911dce18b0456db8
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 11:25:31 +0530

    Add files via upload

---
+++ Co-ordinates.cpp
@@ -0,0 +1,22 @@
+//Program to find coordinates lies in which quadrant
+//Sakshi Sonawane 
+//24070123505
+//Exp-3(2)
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter X coordinates: ";
+    cin>>x;
+    cout<<"Enter Y coordinates: ";
+    cin>>y;
+    if(x>0 && y>0)
+    cout<<"The coordinate lies in first quadrant";
+    else if(x<0 && y>0)
+    cout<<"The coordinate lies in second quadrant";
+    else if(x<0 && y<0)
+    cout<<"The coordinate lies in third quadrant";
+    else
+    cout<<"The coordinate lies in fourth quadrant";
+}
